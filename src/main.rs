4b1b12a220a6288922fb943006264aaf40fd4402
Kyle Simmons <simmonskyleb@gmail.com>
2023-08-23 15:18:33 -0500

    store columns in vecs

--- src/main.rs
+++ src/main.rs
@@ -11,48 +11,85 @@ fn main() {
     };
     
     let column_headers: Vec<String> = headers.split(',').map(|s| s.to_string()).collect();
-    let mut MAFV: Option<usize> = None;
-    let mut MAFI: Option<usize> = None;
-    let mut STFTI: Option<usize> = None;
-    let mut LTFTI: Option<usize> = None;
+    let mut mafvindex: Option<usize> = None;
+    let mut massindex: Option<usize> = None;
+    let mut shortindex: Option<usize> = None;
+    let mut longindex: Option<usize> = None;
 
     for (i, e) in column_headers.into_iter().enumerate() {
         match e.as_str() {
             "MAF Voltage (V)" => {
-                MAFV = Some(i);
+                mafvindex = Some(i);
             }
             "Mass Airflow (g/s)" => {
-                MAFI = Some(i);
+                massindex = Some(i);
             }
             "Short Term FT (%)" => {
-                STFTI = Some(i);
+                shortindex = Some(i);
             }
             "Long Term FT (%)" => {
-                LTFTI = Some(i);
+                longindex = Some(i);
             }
             _ => {}
         }
     }
 
-    if MAFV.is_some() && MAFI.is_some() && STFTI.is_some() && LTFTI.is_some() {
+    if mafvindex.is_some() && massindex.is_some() && shortindex.is_some() && longindex.is_some() {
         println!("All matches were successful!");
-        println!("MAF V index: {:?}", MAFV.unwrap());
-        println!("Mass A index: {:?}", MAFI.unwrap());
-        println!("Short T index: {:?}", STFTI.unwrap());
-        println!("Long T index: {:?}", LTFTI.unwrap());
+        println!("MAF V index: {:?}", mafvindex.unwrap());
+        println!("Mass A index: {:?}", massindex.unwrap());
+        println!("Short T index: {:?}", shortindex.unwrap());
+        println!("Long T index: {:?}", longindex.unwrap());
     } else {
         println!("The following matches were not successful:");
-        if MAFV.is_none() {
+        if mafvindex.is_none() {
             println!("- MAF V");
         }
-        if MAFI.is_none() {
+        if massindex.is_none() {
             println!("- Mass A");
         }
-        if STFTI.is_none() {
+        if shortindex.is_none() {
             println!("- Short T");
         }
-        if LTFTI.is_none() {
+        if longindex.is_none() {
             println!("- Long T");
+        }
+    }
+
+    let mut mafv_values: Vec<f32> = Vec::new();
+    let mut mass_values: Vec<f32> = Vec::new();
+    let mut short_values: Vec<f32> = Vec::new();
+    let mut long_values: Vec<f32> = Vec::new();
+    
+    for line in log.lines().skip(1) {
+        let columns: Vec<&str> = line.split(',').collect();
+    
+        // MAF V
+        if let Some(mafv_value) = columns.get(mafvindex.unwrap()) {
+            if let Ok(value) = mafv_value.parse::<f32>() {
+                mafv_values.push(value);
+            }
+        }
+    
+        // Mass A
+        if let Some(mass_value) = columns.get(massindex.unwrap()) {
+            if let Ok(value) = mass_value.parse::<f32>() {
+                mass_values.push(value);
+            }
+        }
+    
+        // Short T
+        if let Some(short_value) = columns.get(shortindex.unwrap()) {
+            if let Ok(value) = short_value.parse::<f32>() {
+                short_values.push(value);
+            }
+        }
+    
+        // Long T
+        if let Some(long_value) = columns.get(longindex.unwrap()) {
+            if let Ok(value) = long_value.parse::<f32>() {
+                long_values.push(value);
+            }
         }
     }
 }
